377daa159a349cdd5aaf4a582437ad3d6dca19cf
Owen-Cochell <owencochell@gmail.com>
2022-08-29 22:58:44 -0400

    Updates

---
+++ precache-manifest.15d89313d93f405c24b7271d0dea5f4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d3d18aeac5618fa14881",
+    "url": "/HandDetector/static/css/main.8f3ed449.chunk.css"
+  },
+  {
+    "revision": "d3d18aeac5618fa14881",
+    "url": "/HandDetector/static/js/main.fa59a26c.chunk.js"
+  },
+  {
+    "revision": "89342beb3eea9fa3a0c6",
+    "url": "/HandDetector/static/js/runtime~main.c310308e.js"
+  },
+  {
+    "revision": "526d2bdd38e73ccc1fdd",
+    "url": "/HandDetector/static/js/2.ceb55e65.chunk.js"
+  },
+  {
+    "revision": "535a6e537c1ab7a3ee86",
+    "url": "/HandDetector/static/js/3.17534a82.chunk.js"
+  },
+  {
+    "revision": "31094e904fc36a26ca4890e5c3d38275",
+    "url": "/HandDetector/index.html"
+  }
+];
